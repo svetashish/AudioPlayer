b182bd3940081bd2cb484d84ff173f35feb0d604
svetashish <83302938+svetashish@users.noreply.github.com>
2021-11-25 12:41:01 +0300

    add config files

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,54 @@
-import React, {useRef, useState, useEffect} from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import styles from './App.module.css';
-import Slider from "./components/Slider/Slider";
-import Audio from "./components/Audio/Audio";
-import ControlButtons from "./components/ControlButtons/ControlButtons";
+import Slider from './components/Slider/Slider';
+import Audio from './components/Audio/Audio';
+import ControlButtons from './components/ControlButtons/ControlButtons';
 
-const song: string = require('./Tanita_Tikaram_-_Twist_In_My_Sobriety_(musmore.com).mp3')
+const song: string = require('./Tanita_Tikaram_-_Twist_In_My_Sobriety_(musmore.com).mp3');
 
 
 const App = () => {
-    const [isPlaying, setIsPlaying] = useState<boolean>(false)
-    const  audioRef = useRef<HTMLAudioElement| null>(null)
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  const audioRef = useRef<HTMLAudioElement | null>(null);
 
-    const handlePlayPause = () => {
-        const previousValue = !isPlaying
-        setIsPlaying(previousValue)
-        if (previousValue){
-            audioRef.current?.pause();
-        }
-        if (!previousValue){
-            audioRef.current?.play();
-        }
+  const handlePlayPause = () => {
+    const previousValue = !isPlaying;
+    setIsPlaying(previousValue);
+    if (previousValue) {
+      audioRef.current?.pause();
     }
-    const onLoadedMetadata = () => {
-        if (audioRef.current) {
-            console.log("Duration " + audioRef.current.duration);
-        }
-    };
+    if (!previousValue) {
+      audioRef.current?.play();
+    }
+  };
+  const onLoadedMetadata = () => {
+    if (audioRef.current) {
+      console.log('Duration ' + audioRef.current.duration);
+    }
+  };
 
-    // useEffect(() => {
-    //     if (isPlaying) {
-    //         audioRef.current.play();
-    //
-    //     } else {
-    //         audioRef.current.pause();
-    //     }
-    // }, [isPlaying]);
+  // useEffect(() => {
+  //     if (isPlaying) {
+  //         audioRef.current.play();
+  //
+  //     } else {
+  //         audioRef.current.pause();
+  //     }
+  // }, [isPlaying]);
 
 
-    return (
-      <div className={styles.app_wrapper}>
-        <div className={styles.container_player}>
-            <Slider />
-            <ControlButtons audioPlayer={audioRef} isPlaying={isPlaying} onClick={handlePlayPause}/>
-            <Audio
-                src={song}
-                ref={audioRef}
-                onLoadedMetadata={onLoadedMetadata}/>
-        </div>
+  return (
+    <div className={styles.app_wrapper}>
+      <div className={styles.container_player}>
+        <Slider />
+        <ControlButtons audioPlayer={audioRef} isPlaying={isPlaying} onClick={handlePlayPause} />
+        <Audio
+          src={song}
+          ref={audioRef}
+          onLoadedMetadata={onLoadedMetadata} />
       </div>
+    </div>
   );
-}
+};
 
 export default App;
--- src/components/Audio/Audio.tsx
+++ src/components/Audio/Audio.tsx
@@ -1,17 +1,18 @@
-import {forwardRef, SyntheticEvent} from "react";
+import { forwardRef, SyntheticEvent } from 'react';
+
 interface AudioProps {
-    src: string
-    // onLoadedMetadata: SyntheticEvent<HTMLAudioElement>
-    onLoadedMetadata: React.ReactEventHandler<HTMLAudioElement> | undefined
+  src: string;
+  // onLoadedMetadata: SyntheticEvent<HTMLAudioElement>
+  onLoadedMetadata: React.ReactEventHandler<HTMLAudioElement> | undefined;
 }
 
-const Audio = forwardRef<HTMLAudioElement, AudioProps>(({src, onLoadedMetadata}, ref) => {
-    return (
-        <audio
-            src={src}
-            onLoadedMetadata={onLoadedMetadata}
-            ref = {ref}/>
-    )
-})
+const Audio = forwardRef<HTMLAudioElement, AudioProps>(({ src, onLoadedMetadata }, ref) => {
+  return (
+    <audio
+      src={src}
+      onLoadedMetadata={onLoadedMetadata}
+      ref={ref} />
+  );
+});
 
 export default Audio;
